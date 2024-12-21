918261828e118d73881cd033262969a2a31d405b
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2024-12-21 20:27:45 +0530

    url updated

--- linkedin-clone-frontend/src/lib/axios.js
+++ linkedin-clone-frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance  = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://linkedin-ztac.onrender.com",
     withCredentials: true
 })
